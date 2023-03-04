bcd8a4e959f29aaa5de8c83932f63f2a92c858f8
Andrey Soldatenkov <soldatenkov067@bk.ru>
2023-03-04 20:12:24 +0300

    Add hw_6 by OOP course

---
+++ OOP/hw_6/Robot.java
@@ -0,0 +1,11 @@
+package OOP.hw_6;
+
+public interface Robot {
+    public void changeDirection(Direction direction);
+
+    public void move();
+
+    public long getId();
+
+    public Point getPoint();
+}
